bce7961a0f2c7e8de754d1cc1ffc5196ba890254
HuskiGames <baylinclifford8@gmail.com>
2024-06-17 14:33:12 +0930

    push

--- main.py
+++ main.py
@@ -6,23 +6,26 @@ import ProductAccess
 
 def update_listbox(name, index, D = 0):
     global words
+    products = ProductAccess.GetProducts()
     search_term = name.lower()
     if search_term:
         # filtered_words = [word for word in Products.list if  word[0].lower().startswith(search_term)]
         filtered_words = []
         for word in search_term.split():
             if(len(word) > 0):
-                for product in ProductAccess.GetProducts():
+                for product in products:
                     if(word.lower() in product[0].lower()):
                         filtered_words.append(product)
                         
-        for product in ProductAccess.GetProducts():
+        for product in products:
             if(product[2].lower().startswith(search_term.lower())):
                 filtered_words.append(product)
 
         if len(filtered_words) > index:
+            print(filtered_words[index][D])
             return filtered_words[index][D] 
         else:
+            print("Nothing")
             return ""
             
 
@@ -31,8 +34,8 @@ def index():
     return render_template("index.html")
 
 
-# @app.route('/submit', methods=['POST'])
-# def submit():
+@app.route('/submit', methods=['POST'])
+def submit():
     input_data = request.form['input_data']
     return jsonify(
         {'Search_Data_Name_1': update_listbox(input_data, 0, 0),
@@ -70,47 +73,6 @@ def index():
          'Search_Data_Aisle_3': update_listbox(input_data, 2, 5),
          'Search_Data_Aisle_4': update_listbox(input_data, 3, 5),
          'Search_Data_Aisle_5': update_listbox(input_data, 4, 5)
-         })
-
-@app.route('/submit', methods=['POST'])
-def submit():
-    input_data = request.form['input_data']
-    return jsonify(
-        {'Search_Data_Name_1': "",
-         'Search_Data_Name_2': "",
-         'Search_Data_Name_3': "",
-         'Search_Data_Name_4': "",
-         'Search_Data_Name_5': "",
-         
-         'Search_Data_Desc_1': "",
-         'Search_Data_Desc_2': "",
-         'Search_Data_Desc_3': "",
-         'Search_Data_Desc_4': "",
-         'Search_Data_Desc_5': "",
-         
-         'Search_Data_Barcode_1': "",
-         'Search_Data_Barcode_2': "",
-         'Search_Data_Barcode_3': "",
-         'Search_Data_Barcode_4': "",
-         'Search_Data_Barcode_5': "",
-         
-         'Search_Data_Img_1': "",
-         'Search_Data_Img_2': "",
-         'Search_Data_Img_3': "",
-         'Search_Data_Img_4': "",
-         'Search_Data_Img_5': "",
-         
-         'Search_Data_Aisle_Type_1': "",
-         'Search_Data_Aisle_Type_2': "",
-         'Search_Data_Aisle_Type_3': "",
-         'Search_Data_Aisle_Type_4': "",
-         'Search_Data_Aisle_Type_5': "",
-         
-         'Search_Data_Aisle_1': "",
-         'Search_Data_Aisle_2': "",
-         'Search_Data_Aisle_3': "",
-         'Search_Data_Aisle_4': "",
-         'Search_Data_Aisle_5': ""
          })
     
 if __name__ == '__main__':
