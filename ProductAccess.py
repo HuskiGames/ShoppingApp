6a9c50fa622aa1fea036399274b6e8a099e2c04a
HuskiGames <baylinclifford8@gmail.com>
2024-06-17 12:44:47 +0930

    push

--- ProductAccess.py
+++ ProductAccess.py
@@ -1,10 +1,13 @@
 import json
+import os
 
-def SetProducts(list, filename = "Products.json"):
+dir_path = os.path.dirname(os.path.realpath(__file__))
+
+def SetProducts(list, filename = os.path.join(dir_path, "Products.json")):
     with open(filename, 'w') as file:
         json.dump(list, file, indent=3)
 
-def GetProducts(filename = "Products.json"):
+def GetProducts(filename = os.path.join(dir_path, "Products.json")):
     with open(filename, 'r') as file:
         list = json.load(file)
     return list
